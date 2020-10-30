0a108fe33f53a9244849c3990a0406cefe9684f5
sara-benchir <sara.benchir@lepoles.com>
2020-10-30 12:04:13 +0100

    initialisation et récupération

---
+++ [02]ExoRetrouvailles/js/data.js
@@ -0,0 +1,12 @@
+var data = {
+  'perso_0' : {
+    'name' : 'Chouwi',
+    'picture' : 'img/chouwi.jpg',
+    'texte' : ['Brouuuuwhaaaaa','Drrrriiiitch','Hinnn Wouaaa']
+  },
+  'perso_1' : {
+    'name' : 'Archibalde',
+    'picture' : 'img/archi.jpg',
+    'texte' : ['Grand père, c\'est moi!','J\'aime les pâttes moi aussi','Pff nimporte quoi!']
+  }
+}
